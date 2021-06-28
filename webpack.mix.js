c9ee5d4d5092d30f4b7b0abf520244b200e5dce3
Sobral <robson@oficiodesign.com>
2021-06-28 15:32:16 -0300

    Update dependencies, build scripts and paths

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,10 @@
 const mix = require('laravel-mix');
 
 mix
-  .js('resources/scripts/index.js', 'data/html/assets')
-  .sourceMaps()
-  .version()
-  .browserSync();
-
-mix
-  .sass('resources/styles/index.scss', 'data/html/assets')
-  .setPublicPath('data/html/assets')
+  .setPublicPath('data/system/user/templates/default_site/-.group')
+  .js('resources/scripts/index.js', 'scripts.js')
+  .sass('resources/stylesheets/index.scss', 'styles.css')
+  .extract()
   .sourceMaps()
   .version()
   .browserSync();
