AppCivico/gov-tech__ma

commit be83522949fa964eec05b08765cb19b104b8f775

Hide logotype of ExpressionEngine above login form

data/html/admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This source file is part of the open source project
  * ExpressionEngine (https://expressionengine.com)
@@ -22,7 +23,7 @@
  * https://expressionengine.com/expressionengine/user-guide/installation/best_practices.html
  *
  */
-    $system_path = '../system';
+$system_path = '../system';
 
 /*
  * --------------------------------------------------------------------
@@ -59,7 +60,7 @@
  * Enable it only if you have a good reason to.
  *
  */
-    $debug = 0;
+$debug = 0;
 
 /*
  * --------------------------------------------------------------------
@@ -67,20 +68,20 @@
  * --------------------------------------------------------------------
  */
 
-    define('MASKED_CP', true);
+define('MASKED_CP', true);
 
 /*
  * --------------------------------------------------------------------
  *  Resolve the system path for increased reliability
  * --------------------------------------------------------------------
  */
-    $system_path = $system_path ?: __DIR__;
+$system_path = $system_path ?: __DIR__;
 
-    if (realpath($system_path) !== false) {
-        $system_path = realpath($system_path);
-    }
+if (realpath($system_path) !== false) {
+    $system_path = realpath($system_path);
+}
 
-    $system_path = rtrim($system_path, '/') . '/';
+$system_path = rtrim($system_path, '/') . '/';
 
 /*
  * --------------------------------------------------------------------
@@ -88,41 +89,42 @@
  * --------------------------------------------------------------------
  */
 
-    // The name of this file
-    define('SELF', basename(__FILE__)); // Deprecated as of 6.0.0
-    define('EESELF', basename(__FILE__));
+// The name of this file
+define('SELF', basename(__FILE__)); // Deprecated as of 6.0.0
+define('EESELF', basename(__FILE__));
 
-    // Path to this file
-    define('FCPATH', __DIR__ . '/');
+// Path to this file
+define('FCPATH', __DIR__ . '/');
 
-    // Path to the "system" folder
-    define('SYSPATH', $system_path);
+// Path to the "system" folder
+define('SYSPATH', $system_path);
 
-    // Name of the "system folder"
-    define('SYSDIR', basename($system_path));
+// Name of the "system folder"
+define('SYSDIR', basename($system_path));
 
-    // The $debug value as a constant for global access
-    define('DEBUG', $debug);  unset($debug);
+// The $debug value as a constant for global access
+define('DEBUG', $debug);
+unset($debug);
 
-    // The control panel access constant ensures the CP will be invoked.
-    define('REQ', 'CP');
+// The control panel access constant ensures the CP will be invoked.
+define('REQ', 'CP');
 
-    // force the installer/updater?
-    if (file_exists(FCPATH . '.env.php') && (require FCPATH . '.env.php') == true) {
-        define('INSTALL_MODE', getenv('EE_INSTALL_MODE') === 'TRUE');
-    }
+// force the installer/updater?
+if (file_exists(FCPATH . '.env.php') && (require FCPATH . '.env.php') == true) {
+    define('INSTALL_MODE', getenv('EE_INSTALL_MODE') === 'TRUE');
+}
 
 /*
  * --------------------------------------------------------------------
  *  Set the error reporting level
  * --------------------------------------------------------------------
  */
-    if (DEBUG == 1) {
-        error_reporting(E_ALL);
-        @ini_set('display_errors', 1);
-    } else {
-        error_reporting(0);
-    }
+if (DEBUG == 1) {
+    error_reporting(E_ALL);
+    @ini_set('display_errors', 1);
+} else {
+    error_reporting(0);
+}
 
 /*
  *---------------------------------------------------------------
@@ -132,16 +134,20 @@
  * And away we go...
  *
  */
-    // Load the updater package if it's here
-    if (file_exists(SYSPATH . 'ee/updater/boot.php')) {
-        require_once SYSPATH . 'ee/updater/boot.php';
+// Load the updater package if it's here
+if (file_exists(SYSPATH . 'ee/updater/boot.php')) {
+    require_once SYSPATH . 'ee/updater/boot.php';
+}
+// Is the system path correct?
+elseif (!file_exists(SYSPATH . 'ee/ExpressionEngine/Boot/boot.php')) {
+    header('HTTP/1.1 503 Service Unavailable.', true, '503');
+    exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo(__FILE__, PATHINFO_BASENAME));
+} else {
+    require_once SYSPATH . 'ee/ExpressionEngine/Boot/boot.php';
+}
+?><style>
+    .login__logo {
+        display: none;
     }
-    // Is the system path correct?
-    elseif (! file_exists(SYSPATH . 'ee/ExpressionEngine/Boot/boot.php')) {
-        header('HTTP/1.1 503 Service Unavailable.', true, '503');
-        exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo(__FILE__, PATHINFO_BASENAME));
-    } else {
-        require_once SYSPATH . 'ee/ExpressionEngine/Boot/boot.php';
-    }
-
+</style><?php
 // EOF

commit 34385fdb6a087251970cd40db33494855a71d98d

Fix regression caused by be83522949fa964eec05b08765cb19b104b8f775

data/html/admin.php
@@ -145,9 +145,4 @@ elseif (!file_exists(SYSPATH . 'ee/ExpressionEngine/Boot/boot.php')) {
 } else {
     require_once SYSPATH . 'ee/ExpressionEngine/Boot/boot.php';
 }
-?><style>
-    .login__logo {
-        display: none;
-    }
-</style><?php
 // EOF