f43918c19d8a53bae8c4da70ebd4cea02acaad60
Sobral <robson@oficiodesign.com>
2021-07-25 23:26:21 -0300

    Prevent scroll bugs

--- resources/scripts/searchResults/scrollToResults.js
+++ resources/scripts/searchResults/scrollToResults.js
@@ -1,6 +1,7 @@
 export default function scrollToResults() {
   const mainElement = document.querySelector('main, [role="main"]');
 
+  if (document.documentElement.className.indexOf('search') === -1) return;
   if (window.location.hash) return;
 
   if (mainElement) {
