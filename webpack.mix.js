b023872c7f975b0b0c51c5b4e48740c16505bd89
Sobral <robson@oficiodesign.com>
2021-07-29 20:52:35 -0300

    Load layouts and embeds from default site

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix
-  .setPublicPath('data/system/user/templates/default_site/-.group')
+  .setPublicPath('data/system/user/templates/default_site/site.group')
   .js('resources/scripts/index.js', 'scripts.js')
   .sass('resources/stylesheets/index.scss', 'styles.css')
   // .extract()
