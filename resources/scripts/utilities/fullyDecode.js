b064066bb27614262801659f962850028f91d776
sobral <robson@oficiodesign.com>
2021-09-13 19:37:51 -0300

    Fix URI decoder

--- resources/scripts/utilities/fullyDecode.js
+++ resources/scripts/utilities/fullyDecode.js
@@ -1,6 +1,8 @@
 import isEncoded from './isEncoded';
 
 export default ((uri) => {
+  // eslint-disable-next-line no-param-reassign
+  uri = decodeURIComponent(uri.replace(/\+/g, ' '));
   while (isEncoded(uri)) {
     // eslint-disable-next-line no-param-reassign
     uri = decodeURIComponent(uri);
