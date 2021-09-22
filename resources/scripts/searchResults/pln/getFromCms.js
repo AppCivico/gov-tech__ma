a36553d581b703d6094c531d6b4abd5b8142413b
sobral <robson@oficiodesign.com>
2021-09-22 14:45:51 -0300

    Simplify keyword search request

--- resources/scripts/searchResults/pln/getFromCms.js
+++ resources/scripts/searchResults/pln/getFromCms.js
@@ -1,5 +1,3 @@
-const baseUrl = () => `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
-
-export default (searchTerm) => fetch(`${baseUrl()}/busca/pln/${searchTerm}`)
+export default (searchTerm) => fetch(`/busca/pln/${searchTerm}`)
   .then((response) => response.text())
   .then((data) => data);
