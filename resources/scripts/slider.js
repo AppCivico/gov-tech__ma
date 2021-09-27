d78180f86f8f95871cab04a30501e1f7f19b8495
sobral <robson@oficiodesign.com>
2021-09-27 12:35:48 -0300

    Convert last news in home portal into carrousel

--- resources/scripts/slider.js
+++ resources/scripts/slider.js
@@ -5,13 +5,122 @@ export default () => {
     const sliders = document.querySelectorAll('[data-js="slider"]');
 
     for (let i = 0; i < sliders.length; i += 1) {
-      const slider = sliders[i];
-
-      lory(slider, {
-        rewind: true,
+      const sliderEl = sliders[i];
+      const autoPlay = sliderEl.getAttribute('data-slider-play') === 'auto';
+      const slidesToScroll = sliderEl.hasAttribute('data-slides-to-scroll')
+        ? sliderEl.getAttribute('data-slides-to-scroll')
+        : 1;
+      const config = {
         classNameFrame: 'slider__frame',
-        classNameSlideContainer: 'gallery',
-      });
+        classNameSlideContainer: 'slider__container',
+      };
+      const dotContainer = sliderEl.querySelector('.slider__navigation');
+      let lorySlider = null;
+
+      if (autoPlay) {
+        config.enableMouseEvents = true;
+      }
+
+      if (dotContainer) {
+        const dotCount = sliderEl.querySelectorAll('.slider__item').length;
+        const slideTo = (ev) => {
+          lorySlider.slideTo(Array.prototype.indexOf.call(dotContainer.childNodes, ev.target));
+        };
+        const dotListItem = document.createElement('li');
+
+        const handleDotEvent = (e) => {
+          switch (e.type) {
+            case 'before.lory.init':
+              for (let j = 0, len = dotCount; j < len; j += 1) {
+                const clone = dotListItem.cloneNode();
+                dotContainer.appendChild(clone);
+              }
+              dotContainer.childNodes[0].classList.add('active');
+              break;
+
+            case 'after.lory.init':
+              for (let j = 0, len = dotCount; j < len; j += 1) {
+                dotContainer.childNodes[j].addEventListener('click', slideTo);
+              }
+              break;
+
+            case 'after.lory.slide':
+              for (let j = 0, len = dotContainer.childNodes.length; j < len; j += 1) {
+                dotContainer.childNodes[j].classList.remove('active');
+              }
+              dotContainer.childNodes[e.detail.currentSlide - 1].classList.add('active');
+              break;
+
+            case 'on.lory.resize':
+              for (let j = 0, len = dotContainer.childNodes.length; j < len; j += 1) {
+                dotContainer.childNodes[j].classList.remove('active');
+              }
+              dotContainer.childNodes[0].classList.add('active');
+              break;
+
+            default:
+              break;
+          }
+        };
+
+        config.enableMouseEvents = true;
+        config.infinite = slidesToScroll;
+
+        if (dotContainer.hasAttribute('hidden')) {
+          dotContainer.removeAttribute('hidden');
+        }
+
+        sliderEl.addEventListener('before.lory.init', handleDotEvent);
+        sliderEl.addEventListener('after.lory.init', handleDotEvent);
+        sliderEl.addEventListener('after.lory.slide', handleDotEvent);
+        sliderEl.addEventListener('on.lory.resize', handleDotEvent);
+      } else {
+        config.rewind = true;
+      }
+
+      lorySlider = lory(sliderEl, config);
+
+      if (autoPlay) {
+        /**
+         * @link https://codepen.io/nstanard/pen/yaKxzK
+         */
+
+        let stop = false;
+        let fpsInterval;
+        let now;
+        let then;
+        let elapsed;
+
+        const animate = () => {
+          requestAnimationFrame(animate);
+
+          now = Date.now();
+          elapsed = now - then;
+
+          if (elapsed > fpsInterval && !stop) {
+            then = now - (elapsed % fpsInterval);
+            lorySlider.next();
+          }
+        };
+
+        const startAnimating = (fps) => {
+          fpsInterval = 1000 / fps;
+          then = Date.now();
+          animate();
+        };
+
+        const resetTimer = () => {
+          now = Date.now();
+          elapsed = now - then;
+          then = now - (elapsed % fpsInterval);
+        };
+
+        // start the animation process with seed time
+        startAnimating(0.2); // every five seconds
+
+        sliderEl.addEventListener('mouseover', () => { stop = true; });
+        sliderEl.addEventListener('mouseout', () => { resetTimer(); stop = false; });
+      }
     }
   });
 };
