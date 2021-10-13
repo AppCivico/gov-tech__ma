493afebc407dddbef1a18841898aaf1c4984f673
sobral <robson@oficiodesign.com>
2021-10-13 11:23:00 -0300

    Install JEST

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,21 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
+    jest: true,
   },
   extends: [
+    'eslint:recommended',
     'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: [
+    'jest',
+  ],
   root: true,
   rules: {
   },
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globals: {},
+  setupFiles: [],
+  verbose: true,
+};
