78dec822be6b1b5f99279b100e40abfcba913783
sobral <robson@oficiodesign.com>
2021-10-26 21:26:48 -0300

    Install Low Nice Date addon

---
+++ data/system/user/addons/low_nice_date/addon.setup.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'autoload.php';
+$addonJson = json_decode(file_get_contents(__DIR__ . '/addon.json'));
+
+return array(
+    'name'              => $addonJson->name,
+    'description'       => $addonJson->description,
+    'version'           => $addonJson->version,
+    'namespace'         => $addonJson->namespace,
+    'author'            => 'EEHarbor',
+    'author_url'        => 'http://eeharbor.com/low_nice_date',
+    'docs_url'          => 'http://eeharbor.com/low_nice_date/documentation',
+    'settings_exist'    => false,
+);
---
+++ data/system/user/addons/low_nice_date/pi.low_nice_date.php
@@ -0,0 +1,144 @@
+<?php if (! defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+/**
+ * Low Nice Date Plugin class
+ *
+ * @package        low_nice_date
+ * @author         Lodewijk Schutte <hi@gotolow.com>
+ * @link           http://gotolow.com/addons/low-nice-date
+ * @license        http://creativecommons.org/licenses/by-sa/3.0/
+ */
+
+include_once "addon.setup.php";
+use Low\NiceDate\FluxCapacitor\Base\Pi;
+
+class Low_nice_date extends Pi
+{
+
+    // --------------------------------------------------------------------
+    // PROPERTIES
+    // --------------------------------------------------------------------
+
+    /**
+     * Plugin return data
+     *
+     * @var        string
+     */
+    public $return_data;
+
+    // --------------------------------------------------------------------
+    // METHODS
+    // --------------------------------------------------------------------
+
+    /**
+     * Constructor
+     *
+     * @access     public
+     * @return     string
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->return_data = $this->format();
+    }
+
+    /**
+     * Displays the nice date
+     *
+     * @access     public
+     * @return     string
+     */
+    public function format()
+    {
+        // -------------------------------------------
+        // Get parameters
+        // -------------------------------------------
+
+        $date   = ee()->TMPL->fetch_param('date', ee()->localize->now);
+        $format = ee()->TMPL->fetch_param('format');
+        $loc    = (ee()->TMPL->fetch_param('localize') == 'yes');
+
+        // -------------------------------------------
+        // Convert date to timestamp, force GMT/UTC date
+        // -------------------------------------------
+
+        $time = $this->_stamp($date);
+
+        // -------------------------------------------
+        // Format timestamp
+        // -------------------------------------------
+
+        return ($time && $format) ? ee()->localize->format_date($format, $time, $loc) : $date;
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Calculate difference in years, months and days in given date range
+     *
+     * @access     public
+     * @return     string
+     */
+    public function range()
+    {
+        // -------------------------------------------
+        // Get From and To values
+        // -------------------------------------------
+
+        $data = array(
+            'from' => $this->_stamp(ee()->TMPL->fetch_param('from', ee()->localize->now)),
+            'to'   => $this->_stamp(ee()->TMPL->fetch_param('to', ee()->localize->now))
+        );
+
+        // -------------------------------------------
+        // Get absolute difference between the two
+        // -------------------------------------------
+
+        $diff = abs($data['to'] - $data['from']);
+
+        // -------------------------------------------
+        // Get number of years, months and days for this difference
+        // -------------------------------------------
+
+        $data['years']  = floor($diff / (365*60*60*24));
+        $data['months'] = floor(($diff - $data['years'] * 365*60*60*24) / (30*60*60*24));
+        $data['days']   = floor(($diff - $data['years'] * 365*60*60*24 - $data['months']*30*60*60*24) / (60*60*24));
+
+        // -------------------------------------------
+        // Parse the template
+        // -------------------------------------------
+
+        return ee()->TMPL->parse_variables_row(ee()->TMPL->tagdata, $data);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Get timestamp for date
+     *
+     * @access     private
+     * @param      string
+     * @param      bool
+     * @return     int
+     */
+    private function _stamp($str, $utc = true)
+    {
+        if (! is_numeric($str)) {
+            if (is_callable(array(ee()->localize, 'string_to_timestamp'))) {
+                $str = ee()->localize->string_to_timestamp($str);
+            } else {
+                if ($utc) {
+                    $str .= ' UTC';
+                }
+                $str = strtotime($str);
+            }
+        }
+
+        return (int) $str;
+    }
+}
+// END CLASS
+
+/* End of file pi.low_nice_date.php */
