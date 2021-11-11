b4f98d2bb994bf1913c4ddcfe19f463fb059596d
sobral <robson@oficiodesign.com>
2021-11-11 17:43:06 -0300

    Fix bidding search

--- resources/scripts/bidding/getFromCms.js
+++ resources/scripts/bidding/getFromCms.js
@@ -1,6 +1,6 @@
 export default ({ type = '', status = '', year = '' }) => {
-  const segment3 = type;
-  const segment4 = status;
+  const segment3 = type || '-';
+  const segment4 = status || '-';
   let segment5 = Number.parseInt(year, 10) || 0;
   let segment6 = 0;
 
@@ -9,7 +9,7 @@ export default ({ type = '', status = '', year = '' }) => {
     segment6 = Date.UTC(segment5, 11, 31, 23, 59);
   }
 
-  return fetch(`/pregoes/resultados/${segment3}/${segment4}/${segment5 || ''}/${segment6 || ''}`, {
+  return fetch(`/pregoes/resultados/${segment3}/${segment4}/${segment5 || '-'}/${segment6 || '-'}`, {
     method: 'GET',
     mode: 'cors',
     cache: 'default',
