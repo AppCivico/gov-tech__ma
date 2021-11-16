2a2c96bc1e4b8a9b255e5a9692af1848c0c69312
sobral <robson@oficiodesign.com>
2021-11-15 22:48:24 -0300

    Move allowed domains into its own file

---
+++ data/system/user/config/domains.php
@@ -0,0 +1,698 @@
+<?php
+return [
+    [
+        'hosts' => [
+            'aged.ma.gov.br',
+            'aged.ma.gov.local',
+            'aged-ma.appcivico.com.br'
+        ],
+        'site_name' => 'aged',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'mob.ma.gov.br',
+            'mob.ma.gov.local',
+            'mob-ma.appcivico.com.br'
+        ],
+        'site_name' => 'mob',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'agem.ma.gov.br',
+            'agem.ma.gov.local',
+            'agem-ma.appcivico.com.br'
+        ],
+        'site_name' => 'agem',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'homol.agerp.ma.gov.br',
+            'agerp.ma.gov.br',
+            'agerp.ma.gov.local',
+            'agerp-ma.appcivico.com.br'
+        ],
+        'site_name' => 'agerp',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'agemsul.ma.gov.br',
+            'agemsul.ma.gov.local',
+            'agemsul-ma.appcivico.com.br'
+        ],
+        'site_name' => 'agemsul',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'arsema.ma.gov.br',
+            'arsema.ma.gov.local',
+            'arsema-ma.appcivico.com.br'
+        ],
+        'site_name' => 'arsema',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'bombeiros.ma.gov.br',
+            'bombeiros.ma.gov.local',
+            'bombeiros-ma.appcivico.com.br'
+        ],
+        'site_name' => 'bombeiros',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'corona.ma.gov.br',
+            'corona.ma.gov.local',
+            'corona-ma.appcivico.com.br'
+        ],
+        'site_name' => 'corona',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'casacivil.ma.gov.br',
+            'casacivil.ma.gov.local',
+            'casacivil-ma.appcivico.com.br'
+        ],
+        'site_name' => 'casacivil',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'cintra.ma.gov.br',
+            'cintra.ma.gov.local',
+            'cintra-ma.appcivico.com.br'
+        ],
+        'site_name' => 'cintra',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'ccl.ma.gov.br',
+            'ccl.ma.gov.local',
+            'ccl-ma.appcivico.com.br'
+        ],
+        'site_name' => 'ccl',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'caema.ma.gov.br',
+            'caema.ma.gov.local',
+            'caema-ma.appcivico.com.br'
+        ],
+        'site_name' => 'caema',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'consea.ma.gov.br',
+            'consea.ma.gov.local',
+            'consea-ma.appcivico.com.br'
+        ],
+        'site_name' => 'consea',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'defesacivil.ma.gov.br',
+            'defesacivil.ma.gov.local',
+            'defesacivil-ma.appcivico.com.br'
+        ],
+        'site_name' => 'defesacivil',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'detran.ma.gov.br',
+            'transito.ma.gov.br',
+            'detran.ma.gov.local',
+            'transito.ma.gov.local',
+            'detran-ma.appcivico.com.br',
+            'transito-ma.appcivico.com.br'
+        ],
+        'site_name' => 'detran',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'diariooficial.ma.gov.br',
+            'diariooficial.ma.gov.local',
+            'diariooficial-ma.appcivico.com.br'
+        ],
+        'site_name' => 'diariooficial',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'homol.egma.ma.gov.br',
+            'egma.ma.gov.br',
+            'egma.ma.gov.local',
+            'egma-ma.appcivico.com.br'
+        ],
+        'site_name' => 'egma',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'portodoitaqui.ma.gov.br',
+            'emap.ma.gov.br',
+            'portodoitaqui.ma.gov.local',
+            'emap.ma.gov.local',
+            'portodoitaqui-ma.appcivico.com.br',
+            'emap-ma.appcivico.com.br'
+        ],
+        'site_name' => 'portodoitaqui',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'fmrb.ma.gov.br',
+            'fmrb.ma.gov.local',
+            'fmrb-ma.appcivico.com.br'
+        ],
+        'site_name' => 'fmrb',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'fapema.br',
+            'fapema.local',
+            'appcivico.com.br'
+        ],
+        'site_name' => 'fapema',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'gmg.ma.gov.br',
+            'gmg.ma.gov.local',
+            'gmg-ma.appcivico.com.br'
+        ],
+        'site_name' => 'gmg',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'iterma.ma.gov.br',
+            'iterma.ma.gov.local',
+            'iterma-ma.appcivico.com.br'
+        ],
+        'site_name' => 'iterma',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'inmeq.ma.gov.br',
+            'inmeq.ma.gov.local',
+            'inmeq-ma.appcivico.com.br'
+        ],
+        'site_name' => 'inmeq',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'iprev.ma.gov.br',
+            'iprev.ma.gov.local',
+            'iprev-ma.appcivico.com.br'
+        ],
+        'site_name' => 'iprev',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'procon.ma.gov.br',
+            'procon.ma.gov.local',
+            'procon-ma.appcivico.com.br'
+        ],
+        'site_name' => 'procon',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'viva.ma.gov.br',
+            'viva.ma.gov.local',
+            'viva-ma.appcivico.com.br'
+        ],
+        'site_name' => 'viva',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'imesc.ma.gov.br',
+            'imesc.ma.gov.local',
+            'imesc-ma.appcivico.com.br'
+        ],
+        'site_name' => 'imesc',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'jucema.ma.gov.br',
+            'jucema.ma.gov.local',
+            'jucema-ma.appcivico.com.br'
+        ],
+        'site_name' => 'jucema',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'lacen.ma.gov.br',
+            'lacen.ma.gov.local',
+            'lacen-ma.appcivico.com.br'
+        ],
+        'site_name' => 'lacen',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'homol.mapa.ma.gov.br',
+            'mapa.ma.gov.br',
+            'mapa.ma.gov.local',
+            'mapa-ma.appcivico.com.br'
+        ],
+        'site_name' => 'mapa',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'policiacivil.ma.gov.br',
+            'policiacivil.ma.gov.local',
+            'policiacivil-ma.appcivico.com.br'
+        ],
+        'site_name' => 'policiacivil',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'pm.ssp.ma.gov.br',
+            'pm.ssp.ma.gov.local',
+            'pm-ssp.ma.appcivico.com.br'
+        ],
+        'site_name' => 'pm',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'transparencia.ma.gov.br',
+            'transparencia.ma.gov.local',
+            'transparencia-ma.appcivico.com.br'
+        ],
+        'site_name' => 'transparencia',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'pge.ma.gov.br',
+            'pge.ma.gov.local',
+            'pge-ma.appcivico.com.br'
+        ],
+        'site_name' => 'pge',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'trabalhojovem.ma.gov.br',
+            'trabalhojovem.ma.gov.local',
+            'trabalhojovem-ma.appcivico.com.br'
+        ],
+        'site_name' => 'trabalhojovem',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'minhacasamelhor.ma.gov.br',
+            'minhacasamelhor.ma.gov.local',
+            'minhacasamelhor-ma.appcivico.com.br'
+        ],
+        'site_name' => 'minhacasamelhor',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'radiotimbira.ma.gov.b',
+            'radiotimbira.ma.gov.local',
+            'radiotimbira-ma.appcivico.com.br'
+        ],
+        'site_name' => 'radiotimbira',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seati.ma.gov.br',
+            'seati.ma.gov.local',
+            'seati-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seati',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'secap.ma.gov.br',
+            'secap.ma.gov.local',
+            'secap-ma.appcivico.com.br'
+        ],
+        'site_name' => 'secap',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sagrima.ma.gov.br',
+            'sagrima.ma.gov.local',
+            'sagrima-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sagrima',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'secti.ma.gov.br',
+            'secti.ma.gov.local',
+            'secti-ma.appcivico.com.br'
+        ],
+        'site_name' => 'secti',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'secom.ma.gov.br',
+            'secom.ma.gov.local',
+            'secom-ma.appcivico.com.br'
+        ],
+        'site_name' => 'secom',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sefaz.ma.gov.br',
+            'sefaz.ma.gov.local',
+            'sefaz-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sefaz',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'segep.ma.gov.br',
+            'segep.ma.gov.local',
+            'segep-ma.appcivico.com.br'
+        ],
+        'site_name' => 'segep',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'mulher.ma.gov.br',
+            'semu.ma.gov.br',
+            'mulher.ma.gov.local',
+            'semu.ma.gov.local',
+            'mulher-ma.appcivico.com.br',
+            'semu-ma.appcivico.com.br'
+        ],
+        'site_name' => 'semu',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'saude.ma.gov.br',
+            'saude.ma.gov.local',
+            'saude-ma.appcivico.com.br'
+        ],
+        'site_name' => 'saude',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'secid.ma.gov.br',
+            'secid.ma.gov.local',
+            'secid-ma.appcivico.com.br'
+        ],
+        'site_name' => 'secid',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seap.ma.gov.br',
+            'seap.ma.gov.local',
+            'seap-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seap',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'saf.ma.gov.br',
+            'saf.ma.gov.local',
+            'saf-ma.appcivico.com.br'
+        ],
+        'site_name' => 'saf',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'cultura.ma.gov.br',
+            'secma.ma.gov.br',
+            'cultura.ma.gov.local',
+            'secma.ma.gov.local',
+            'cultura-ma.appcivico.com.br',
+            'secma-ma.appcivico.com.br'
+        ],
+        'site_name' => 'secma',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sedes.ma.gov.br',
+            'sedes.ma.gov.local',
+            'sedes-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sedes',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'homol.sedihpop.ma.gov.br',
+            'sedihpop.ma.gov.br',
+            'direitos.ma.gov.local',
+            'sedihpop.ma.gov.local',
+            'direitos-ma.appcivico.com.br',
+            'sedihpop-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sedihpop',
+        'editorial_group' => '21'
+    ],
+    [
+        'hosts' => [
+            'educacao.ma.gov.br',
+            'seduc.ma.gov.br',
+            'educacao.ma.gov.local',
+            'seduc.ma.gov.local',
+            'educacao-ma.appcivico.com.br',
+            'seduc-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seduc',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sedel.ma.gov.br',
+            'sedel.ma.gov.local',
+            'sedel-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sedel',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'segov.ma.gov.br',
+            'segov.ma.gov.local',
+            'segov-ma.appcivico.com.br'
+        ],
+        'site_name' => 'segov',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seinc.ma.gov.br',
+            'seinc.ma.gov.local',
+            'seinc-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seinc',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sinfra.ma.gov.br',
+            'sinfra.ma.gov.local',
+            'sinfra-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sinfra',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sema.ma.gov.br',
+            'sema.ma.gov.local',
+            'sema-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sema',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seme.ma.gov.br',
+            'seme.ma.gov.local',
+            'seme-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seme',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sepe.ma.gov.br',
+            'sepe.ma.gov.local',
+            'sepe-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sepe',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'sri.ma.gov.br',
+            'sri.ma.gov.local',
+            'sri-ma.appcivico.com.br'
+        ],
+        'site_name' => 'sri',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'rebras.ma.gov.br',
+            'rebras.ma.gov.local',
+            'rebras-ma.appcivico.com.br'
+        ],
+        'site_name' => 'rebras',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'homol.turismo.ma.gov.br',
+            'turismo.ma.gov.br',
+            'setur.ma.gov.br',
+            'turismo.ma.gov.local',
+            'setur.ma.gov.local',
+            'turismo-ma.appcivico.com.br',
+            'setur-ma.appcivico.com.br'
+        ],
+        'site_name' => 'setur',
+        'editorial_group' => '9'
+    ],
+    [
+        'hosts' => [
+            'ssp.ma.gov.br',
+            'ssp.ma.gov.local',
+            'ssp-ma.appcivico.com.br'
+        ],
+        'site_name' => 'ssp',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'stc.ma.gov.br',
+            'stc.ma.gov.local',
+            'stc-ma.appcivico.com.br'
+        ],
+        'site_name' => 'stc',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seplan.ma.gov.br',
+            'seplan.ma.gov.local',
+            'seplan-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seplan',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'trabalho.ma.gov.br',
+            'setres.ma.gov.br',
+            'trabalho.ma.gov.local',
+            'setres.ma.gov.local',
+            'trabalho-ma.appcivico.com.br',
+            'setres-ma.appcivico.com.br'
+        ],
+        'site_name' => 'setres',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'juventude.ma.gov.br',
+            'seejuv.ma.gov.br',
+            'juventude.ma.gov.local',
+            'seejuv.ma.gov.local',
+            'juventude-ma.appcivico.com.br',
+            'seejuv-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seejuv',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seepp.ma.gov.br',
+            'seepp.ma.gov.local',
+            'seepp-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seepp',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'igualdaderacial.ma.gov.br',
+            'seir.ma.gov.br',
+            'igualdaderacial.ma.gov.local',
+            'seir.ma.gov.local',
+            'igualdaderacial-ma.appcivico.com.br',
+            'seir-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seir',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'seloquilombos.ma.gov.br',
+            'seloquilombos.ma.gov.local',
+            'seloquilombos-ma.appcivico.com.br'
+        ],
+        'site_name' => 'seloquilombos',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'tce.ma.gov.br',
+            'tce.ma.gov.local',
+            'tce-ma.appcivico.com.br'
+        ],
+        'site_name' => 'tce',
+        'editorial_group' => ''
+    ],
+    [
+        'hosts' => [
+            'homol.ma.gov.br',
+            'ma.gov.br',
+            'ma.gov.local',
+            'ma.appcivico.com.br',
+            'localhost',
+        ],
+        'site_name' => 'default_site',
+        'editorial_group' => '3'
+    ]
+];
+
+// EOF
