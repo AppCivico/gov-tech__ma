b214a79c095d5d745f44ccc5465e0065f2fb9f69
Sobral <robson@oficiodesign.com>
2021-11-29 16:26:15 -0300

    Remove query selector from navigation script

--- resources/scripts/selectNavigation.js
+++ resources/scripts/selectNavigation.js
@@ -1,27 +1,36 @@
 export default (() => {
-  const navigationForms = document.querySelectorAll('[data-js="select-navigation"]');
+  const navigationForms = document.forms;
+
+  console.log(navigationForms.length);
+
+  document.body.innerHTML += `<pre>${navigationForms.length}</pre>`;
 
   for (let i = 0; i < navigationForms.length; i += 1) {
     const form = navigationForms[i];
 
-    if (form.hasAttribute('disabled')) {
-      form.removeAttribute('disabled');
+    if (form.getAttribute('data-js') === 'select-navigation') {
+      if (form.hasAttribute('disabled')) {
+        form.removeAttribute('disabled');
+      }
+
+      if (form.hasAttribute('hidden')) {
+        form.removeAttribute('hidden');
+      }
+
+      form.addEventListener('submit', (e) => {
+        if (!(e instanceof Event)) return;
+        if (!(e.currentTarget instanceof HTMLFormElement)) return;
+
+        const locationEl = e.currentTarget.elements['location-to'];
+        const { value } = locationEl || locationEl.options[locationEl.selectedIndex];
+
+        // turn relative URLs into absolute
+        const proxyAnchor = document.createElement('a');
+        proxyAnchor.href = value;
+
+        window.location.href = proxyAnchor.href;
+        e.preventDefault();
+      });
     }
-
-    if (form.hasAttribute('hidden')) {
-      form.removeAttribute('hidden');
-    }
-
-    form.addEventListener('submit', (e) => {
-      const { location: locationEl } = e.target.elements;
-      const { value } = locationEl || locationEl.options[locationEl.selectedIndex];
-
-      // turn relative URLs into absolute
-      const proxyAnchor = document.createElement('a');
-      proxyAnchor.href = value;
-
-      window.location = proxyAnchor.href;
-      e.preventDefault();
-    });
   }
 });
