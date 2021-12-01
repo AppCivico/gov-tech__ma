0b26352a545238ee94838a64fac66fd3c6b15c71
Sobral <robson@oficiodesign.com>
2021-11-30 21:54:23 -0300

    Update layout of Juçara

--- resources/scripts/flag-first-access.js
+++ resources/scripts/flag-first-access.js
@@ -1,8 +1,28 @@
+function registerFirstAccess() {
+  const firstVisit = new Date().getTime();
+
+  window.localStorage.setItem('firstVisit', String(firstVisit));
+  document.documentElement.className += ' first-access';
+}
+
+function checkFirstOpening(e) {
+  if (e instanceof Event && e.target instanceof HTMLElement) {
+    switch (true) {
+      case (e.target.className.indexOf('sc-custom-open-icon') > -1):
+      case (e.target.className.indexOf('sc-launcher') > -1):
+        document.documentElement.classList.remove('first-access');
+        document.documentElement.removeEventListener('click', checkFirstOpening);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
 export default (() => {
   if (!localStorage?.firstVisit || window.location.search.indexOf('first-access') !== -1) {
-    const firstVisit = new Date().getTime();
-
-    window.localStorage.setItem('firstVisit', String(firstVisit));
-    document.documentElement.className += ' first-access';
+    registerFirstAccess();
+    document.documentElement.addEventListener('click', checkFirstOpening);
   }
 });
