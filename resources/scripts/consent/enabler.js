bc9f3ca74dccad1ba2c3abdef801305e786a2ab4
Sobral <robson@oficiodesign.com>
2021-12-02 18:46:08 -0300

    Hide consent required features

---
+++ resources/scripts/consent/enabler.js
@@ -0,0 +1,19 @@
+export default function enabler() {
+  const consents = window['ma:consents']?.split(' ');
+
+  if (consents.length) {
+    consents.forEach((consent) => {
+      const items = document.querySelectorAll(`[data-js="${consent}"]`);
+
+      for (let i = 0; i < items.length; i += 1) {
+        const item = items[i];
+        if (item.hasAttribute('hidden')) {
+          item.removeAttribute('hidden');
+        }
+        if (item.hasAttribute('disabled')) {
+          item.removeAttribute('disabled');
+        }
+      }
+    });
+  }
+}
