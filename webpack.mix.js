469477a22b4d885e820266d9611990682ee637f1
Sobral <robson@oficiodesign.com>
2021-12-16 14:00:46 -0300

    Disable generation of source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,6 @@ mix
   .setPublicPath('data/system/user/templates/default_site/site.group')
   .js('resources/scripts/index.js', 'scripts.js')
   .sass('resources/stylesheets/index.scss', 'styles.css')
-  .sourceMaps()
   .version();
 
 mix
